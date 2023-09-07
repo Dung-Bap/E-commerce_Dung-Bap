e1d001aede44fa7b9cf393a1a02cbb95ff2dda08
Dung_Bap <ngovandung397@gmail.com>
2023-09-07 10:16:38 +0700

    css FeaturedProduct

--- client/src/pages/public/Home.js
+++ client/src/pages/public/Home.js
@@ -1,20 +1,23 @@
 /** @format */
 
 import React from 'react';
-import { Banner, BestSeller, Sidebar, DailyDeal } from '../../components';
+import { Banner, BestSeller, Sidebar, DailyDeal, FeaturedProduct } from '../../components';
 
 const Home = () => {
     return (
-        <div className="w-main mt-[20px] flex">
-            <div className="flex flex-col w-[25%] ">
-                <Sidebar />
-                <DailyDeal />
+        <>
+            <div className="w-main mt-[20px] flex">
+                <div className="flex flex-col w-[25%] ">
+                    <Sidebar />
+                    <DailyDeal />
+                </div>
+                <div className="flex flex-col pl-5 w-[75%] ">
+                    <Banner />
+                    <BestSeller />
+                </div>
             </div>
-            <div className="flex flex-col pl-5 w-[75%] ">
-                <Banner />
-                <BestSeller />
-            </div>
-        </div>
+            <FeaturedProduct />
+        </>
     );
 };
 
